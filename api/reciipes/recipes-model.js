f1fa1379a42a3b2641c97bfd1a57c87241682c8d
abe-one <77995299+abe-one@users.noreply.github.com>
2021-05-13 22:56:20 -0500

    begin db call

--- api/reciipes/recipes-model.js
+++ api/reciipes/recipes-model.js
@@ -1,5 +1,15 @@
 const db = require("../../data/db-config");
 
-exports.getRecipeById = (id) => {
-  return db("recipes");
+exports.getRecipeById = async (id) => {
+  // const recipeSteps = await
+  return db
+    .select("r.recipe_name", "r.recipe_creation_date", "s.*")
+    .from("recipes as r")
+    .join("steps as s", function () {
+      this.on("r.recipe_id", "s.recipe_id");
+    })
+    .where("r.recipe_id", 2)
+    .orderBy("s.step_number");
+
+  // return recipeSteps;
 };
